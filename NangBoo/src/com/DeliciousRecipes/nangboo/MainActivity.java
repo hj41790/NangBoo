8f4da6114abd58e876eb9e158443a597fe23b951
hj41790 <hj41790@naver.com>
2015-08-03 18:56:49 +0900

    드디어 정리끝

--- NangBoo/src/com/DeliciousRecipes/nangboo/MainActivity.java
+++ NangBoo/src/com/DeliciousRecipes/nangboo/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends Activity {
 		mListView.setDividerHeight(2);
 		mListView.setAdapter(mAdapter);
 
+		// 리스트뷰 아이템 클릭 리스너
 		mListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
@@ -82,7 +83,10 @@ public class MainActivity extends Activity {
 				if (isClicked_chooseButton) {
 					mAdapter.itemChoosed(a);
 				} else {
-					createItemSelectedPopup(a);
+					
+					Intent intent = new Intent(MainActivity.this, ModifyActivity.class);
+					startActivity(intent);
+					//createItemSelectedPopup(a);
 				}
 			}
 		});
@@ -128,10 +132,6 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		/*
-		 * 여기부터 경원이가 인텐트 해본다고 추가한거 ^-^! 된당!!!!!!!!! 다른 버튼에도 리스너~>인텐트
-		 * 넣어주기^ㅇ^ㅇ!!!!!!!!!
-		 */
 		addIngredient.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -178,81 +178,6 @@ public class MainActivity extends Activity {
 		});
 
 		mDialog.show();
-
-	}
-
-	private void createItemSelectedPopup(int a) {
-
-		isModifyBtnSelected = false;
-		position = a;
-
-		/* 레이아웃 수집 */
-		popupLayout = getLayoutInflater().inflate(R.layout.layout_modify_item,
-				null);
-
-		modifyBtn = (Button) popupLayout
-				.findViewById(R.id.modifyButton_modifyItem);
-		closeBtn = (Button) popupLayout
-				.findViewById(R.id.closeButton_modifyItem);
-
-		name = (EditText) popupLayout.findViewById(R.id.name_modifyItem);
-		expiration = (EditText) popupLayout
-				.findViewById(R.id.expiration_modifyItem);
-		memo = (EditText) popupLayout.findViewById(R.id.memo_modifyItem);
-
-		/* 정보 불러오기 */
-		Ingredient tmp = (Ingredient) mAdapter.getItem(position);
-
-		name.setText(tmp.name);
-		expiration.setText(tmp.expirationDate);
-		memo.setText(tmp.memo);
-
-		/* 수정버튼 클릭리스너 */
-		modifyBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				isModifyBtnSelected = !isModifyBtnSelected;
-
-				if (isModifyBtnSelected) { // 수정모드 활성화
-					modifyBtn.setText("확인");
-					modifyBtn
-							.setBackgroundResource(R.drawable.btn_default_focused_holo_light);
-					name.setEnabled(true);
-					expiration.setEnabled(true);
-					memo.setEnabled(true);
-				} else { // 수정모드 비활성화
-					modifyBtn.setText("수정");
-					modifyBtn
-							.setBackgroundResource(R.drawable.btn_default_normal_holo_light);
-					name.setEnabled(false);
-					expiration.setEnabled(false);
-					memo.setEnabled(false);
-
-					Ingredient tmp = new Ingredient();
-					tmp.name = name.getText().toString();
-					tmp.expirationDate = expiration.getText().toString();
-					tmp.memo = memo.getText().toString();
-
-					mAdapter.modify(position, tmp);
-				}
-			}
-		});
-
-		/* 닫기버튼 클릭리스너 */
-		closeBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				popupWindow.dismiss();
-			}
-		});
-
-		/* 팝업 윈도우 생성 및 출력 */
-		popupWindow = new PopupWindow(popupLayout, LayoutParams.WRAP_CONTENT,
-				LayoutParams.WRAP_CONTENT, true);
-		popupWindow.setBackgroundDrawable(new ColorDrawable());
-		popupWindow.showAsDropDown(findViewById(R.id.title_main), 15, 100);
 
 	}
 }
