716de512ec90d9769d3b4be2a37c0e3586ceab6c
hj41790 <hj41790@naver.com>
2015-08-25 03:56:13 +0900

    즐겨찾기추가 틀 완성

--- NangBoo/src/com/DeliciousRecipes/nangboo/WebviewActivity.java
+++ NangBoo/src/com/DeliciousRecipes/nangboo/WebviewActivity.java
@@ -16,10 +16,13 @@ import android.widget.Toast;
 
 public class WebviewActivity extends Activity{
 
+	AddBookmark_Dialog mDialog;
 	
 	Button back, bookmark;
 	WebView webview;
 	ProgressBar progressBar;
+	
+	String currentURL;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,6 +32,7 @@ public class WebviewActivity extends Activity{
 		Bundle inputData = intent.getBundleExtra("SEARCHING_URL");
 		
 		String url = inputData.getString("URL");
+		currentURL = url;
 
 		back = (Button) findViewById(R.id.back_button_webview);
 		bookmark = (Button) findViewById(R.id.add_bookmark_button);
@@ -40,7 +44,14 @@ public class WebviewActivity extends Activity{
 		webview.loadUrl(url);
 		webview.setWebViewClient(new WebViewClient(){
 			public boolean shoudOverridUrlLoading(WebView view, String url){
-				view.loadUrl(url);
+				
+				if(currentURL != null && url.equals(currentURL)){
+					webview.goBack();
+				}
+				else{
+					view.loadUrl(url);
+					currentURL = url;
+				}
 				return true;
 			}
 
@@ -80,7 +91,7 @@ public class WebviewActivity extends Activity{
 
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(WebviewActivity.this, "즐겨찾기에 추가되었습니다.",Toast.LENGTH_SHORT).show();
+				createAddBookmarkDialog();
 			}
 			
 		});
@@ -102,5 +113,55 @@ public class WebviewActivity extends Activity{
 		return super.onKeyDown(keyCode, event);
 	}
 
+	private void createAddBookmarkDialog(){
+		mDialog = new AddBookmark_Dialog(this);
+		
+		mDialog.setTitle("즐겨찾기 추가");
+		
+		mDialog.setOKClickListener(new View.OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				
+				if(mDialog.isEditTextFilled()){
+
+					mDialog.dismiss();
+					Toast.makeText(WebviewActivity.this, "즐겨찾기에 추가되었습니다.", Toast.LENGTH_SHORT).show();
+					
+				}
+				else Toast.makeText(WebviewActivity.this, "제목을 입력하세요.", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		mDialog.setCancelListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mDialog.dismiss();
+			}
+		});
+
+		mDialog.show();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
